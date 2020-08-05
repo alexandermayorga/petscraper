174eeb6524232a1f1bd44fb7b588c5e1f016063a
Alex Mayorga <ninjas@3menmovers.com>
2020-08-04 19:43:48 -0500

    Maybe it was a typo all along -.-

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, './client/build')));
 
 app.use('/links', indexRouter);
 app.use('/pet', petRouter);
